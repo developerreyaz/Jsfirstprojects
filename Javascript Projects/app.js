75c6808e08a0eb78a0427f1e0bf68b04cca04168
developerreyaz <78953062+developerreyaz@users.noreply.github.com>
2021-04-07 15:30:37 +0600

    Add files via upload

---
+++ Javascript Projects/app.js
@@ -0,0 +1,11 @@
+
+///First of all crete a Variable
+var Fname = prompt("What's your name?");
+var Lname = prompt("What's your lover name?");
+
+//Create a random number
+let number = Math.random() * 100;
+number = Math.floor(number) + 1;
+//Create a alert button
+alert(Fname + " and " + Lname + " Love percentage is: " + number + "%");
+
